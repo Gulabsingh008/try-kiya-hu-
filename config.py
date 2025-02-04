0b3bb303cec3b735458553c60712924aeece2b29
AF <gulabsinghrajput008@gmail.com>
2025-02-04 15:42:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # config.py
 
 BOT_TOKEN = "7981510007:AAHX8_AhG5tja-yEZN1KOR-1klFGYt0mkLA"
+API_ID = "26494161"  # Replace with your API ID
+API_HASH = "55da841f877d16a3a806169f3c5153d3"  # Replace with your API Hash
